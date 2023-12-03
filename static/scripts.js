6a3f94fd6c7c7b4efc162d644c9770340311945d
GrainedCube2214 <80443280+GrainedCube2214@users.noreply.github.com>
2023-12-03 13:32:27 +0530

    Unified theme switching

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,5 @@
+// scripts.js
+
 function toggleTheme() {
     const body = document.body;
     const themeIcon = document.getElementById('themeIcon');
@@ -7,9 +9,34 @@ function toggleTheme() {
     body.classList.toggle("dark-mode");
 
     // Toggle the visibility of the theme icons
-    themeIcon.style.display = themeIcon.style.display === 'none' ? 'inline-block' : 'none';
-    themeIconDark.style.display = themeIconDark.style.display === 'none' ? 'inline-block' : 'none';
+    if (body.classList.contains('dark-mode')) {
+        themeIcon.style.display = 'none';
+        themeIconDark.style.display = 'inline-block';
+    } else {
+        themeIcon.style.display = 'inline-block';
+        themeIconDark.style.display = 'none';
+    }
+
+    // Store the selected theme in a cookie or local storage
+    const selectedTheme = body.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('selectedTheme', selectedTheme);
 }
+
+// Function to set the initial theme
+function setInitialTheme() {
+    const body = document.body;
+    const selectedTheme = localStorage.getItem('selectedTheme');
+
+    if (selectedTheme === 'dark') {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+}
+
+// Set the initial theme when the script loads
+setInitialTheme();
+
 function goBack() {
     window.history.back();
 }
